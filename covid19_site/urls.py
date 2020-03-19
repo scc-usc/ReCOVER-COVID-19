d312d295b1df0f2d0b44a530602a7d4def362120
Jamin Chen <jaminche@usc.edu>
2020-03-18 22:44:09 -0700

    Jk now it's set up.

--- covid19_site/urls.py
+++ covid19_site/urls.py
@@ -1,4 +1,4 @@
-"""modelAPI URL Configuration
+"""covid19_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
