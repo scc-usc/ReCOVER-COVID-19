5b0964f434ed76d77df37a55bdbd39d1dfa026ee
Jamin Chen <jaminche@usc.edu>
2020-05-02 18:38:16 -0700

    Add 'models' endpoint.

--- model_api/urls.py
+++ model_api/urls.py
@@ -5,6 +5,7 @@ from model_api import views
 urlpatterns = [
     path('affected_by/', views.affected_by),
     path('areas/', views.areas),
+    path('cumulative_infections/', views.cumulative_infections),
     path('predict/', views.predict),
-    path('cumulative_infections/', views.cumulative_infections,)
+    path('models/', views.models)
 ]
