84eb057b7f45196082871cc2360b45c4b3b749c2
Tianjian Xu <xutianjian@gmail.com>
2020-05-24 00:39:32 -0700

    Create "about us" page and navigation tabs

--- frontend/src/covid19app.js
+++ frontend/src/covid19app.js
@@ -1,57 +1,41 @@
 import React, { Component } from "react";
 import Covid19Predict from "./covid19predict";
+import AboutUS from "./aboutus"
 import { 
-  Layout, 
-  Menu,
+  Layout,
   Row,
-  Col, 
-  Breadcrumb 
+  Col,
+  Tabs
 } from 'antd';
 import "./covid19app.css";
 
 const { Header, Content, Footer } = Layout;
 
-const navigate = ({key}) => {
-  if (key == 'COVID-19 Forecast') {
-    document.getElementById("main-content").innerHTML = "<Covid19Predict />";
-  } else if (key == 'About Us') {
-    document.getElementById("main-content").innerHTML = "<p>hello!</p>;
-  }
-}
+const { TabPane } = Tabs;
 
 class Covid19App extends Component {
+
   render() {
     return (
+
       <div className="app-wrapper">
         <Layout className="app-layout">
-          <Header>
-            <Row>
-            <Col >
+          <Tabs className="nav-bar" defaultActiveKey="1" type="card">
+            <TabPane tab= {
               <img 
                 className="logo"
-                src="https://identity.usc.edu/files/2011/12/combo_gold_white_cardinal.png" 
+                src="https://identity.usc.edu/files/2011/12/combo_gold_white_cardinal.png"
                 alt="USC"
               />
-              </Col>
-              <Col>
-                <Menu theme="dark"
-                 mode="horizontal" 
-                 defaultSelectedKeys={['COVID-19 Forecast']} 
-                 onClick={navigate}>
-                  <Menu.Item key="COVID-19 Forecast">COVID-19 Forecast</Menu.Item>
-                  <Menu.Item key="How to Use">How to Use</Menu.Item>
-                  <Menu.Item key="About Us">About Us</Menu.Item>
-                </Menu>
-              </Col>
-            </Row>
-            
-          </Header>
-
-          <Content className="main-content" id="main-content">
-            <Covid19Predict />
-          </Content>
+            }></TabPane>
+            <TabPane tab="Covid-19 Forecast" key="1">
+              <Covid19Predict />
+            </TabPane>
+            <TabPane tab="About us" key="2">
+              <AboutUS />
+            </TabPane>
+          </Tabs>
         </Layout>
-        
       </div>
     );
   }
