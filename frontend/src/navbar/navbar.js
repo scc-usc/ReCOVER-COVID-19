f9b66f3d587c20827e31e6888c59acd5cf2f623c
Xiaochen Yang <xiaochey@usc.edu>
2020-05-24 20:12:07 -0700

    routing

---
+++ frontend/src/navbar/navbar.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import {
+    Menu,
+    Row,
+    Col
+} from 'antd';
+import "./navbar.css";
+
+class NavBar extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            activateItem: 'forecast'
+        };
+    }
+
+    handleItemClick = (e) => 
+    {
+        const {redirectForecast, redirectInstruction, redirectAbout} = this.props;
+        console.log(e);
+        const {key} = e;
+        this.setState({
+            activeItem: e.key
+        });
+        if (key === "forecast")
+        {
+            redirectForecast();
+        }
+        else if (key === "information")
+        {
+            redirectAbout();
+        }
+        else if (key === "instruction")
+        {
+            redirectInstruction();
+        }
+    }
+
+    render() {
+        return (
+            <Row className="navbar-container">
+            <Col >
+                <img
+                    className="logo"
+                    src="https://identity.usc.edu/files/2011/12/combo_gold_white_cardinal.png"
+                    alt="USC"
+                />
+            </Col>
+            <Col>
+                <Menu theme="light"
+                    mode="horizontal"
+                    onClick={this.handleItemClick}
+                    defaultSelectedKeys={['forecast']}>
+                    <Menu.Item key="forecast">COVID-19 Forecast</Menu.Item>
+                    <Menu.Item key="instruction">How to Use</Menu.Item>
+                    <Menu.Item key="information">About Us</Menu.Item>
+                </Menu>
+            </Col>
+        </Row>
+        );
+    }
+}
+
+export default NavBar;
