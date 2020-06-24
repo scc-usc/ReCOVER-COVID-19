e93493e38204f64da9648bb2753bd92789044b64
Xiaochen Yang <xiaochey@usc.edu>
2020-06-24 15:21:17 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReCOVER-COVID-19/precache-manifest.b432dfefdb439c0c889925af2cf664a9.js"
+  "/ReCOVER-COVID-19/precache-manifest.2568c206419d326223448d554561ad14.js"
 );
 
 self.addEventListener('message', (event) => {
