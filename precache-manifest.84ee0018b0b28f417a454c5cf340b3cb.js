33a132d6aa40e9bf21c71fc9c6824fc8dc58e835
Xiaochen Yang <xiaochey@usc.edu>
2020-06-27 15:54:36 -0700

    Updates

---
+++ precache-manifest.84ee0018b0b28f417a454c5cf340b3cb.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c49e548d906e51df9bf7ca47ec58a53",
+    "url": "/ReCOVER-COVID-19/index.html"
+  },
+  {
+    "revision": "68d8240352a88817fd0d",
+    "url": "/ReCOVER-COVID-19/static/css/3.fca20ba5.chunk.css"
+  },
+  {
+    "revision": "933c99d59dfee5372d41",
+    "url": "/ReCOVER-COVID-19/static/css/main.07b3d387.chunk.css"
+  },
+  {
+    "revision": "68d8240352a88817fd0d",
+    "url": "/ReCOVER-COVID-19/static/js/3.b8ca9c7b.chunk.js"
+  },
+  {
+    "revision": "6a235d09beb5db87c57af2ef5ef29298",
+    "url": "/ReCOVER-COVID-19/static/js/3.b8ca9c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe0e93aca6e9ee02f1cf",
+    "url": "/ReCOVER-COVID-19/static/js/4.f222132b.chunk.js"
+  },
+  {
+    "revision": "c6643d9358ed307ac794",
+    "url": "/ReCOVER-COVID-19/static/js/5.e914a5ae.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/ReCOVER-COVID-19/static/js/5.e914a5ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "707363bb7943dad9640e",
+    "url": "/ReCOVER-COVID-19/static/js/6.35451e96.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/ReCOVER-COVID-19/static/js/6.35451e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933c99d59dfee5372d41",
+    "url": "/ReCOVER-COVID-19/static/js/main.bc21caea.chunk.js"
+  },
+  {
+    "revision": "c90d0fbe82d6b1b2f0d3",
+    "url": "/ReCOVER-COVID-19/static/js/runtime-main.4414e7c5.js"
+  },
+  {
+    "revision": "a96a1a154c868628288e",
+    "url": "/ReCOVER-COVID-19/static/js/xlsx.501e8aa2.chunk.js"
+  },
+  {
+    "revision": "bc502e3fa82ff72a5c2fd6c7bfeb9acc",
+    "url": "/ReCOVER-COVID-19/static/media/all_graph.bc502e3f.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/ReCOVER-COVID-19/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/ReCOVER-COVID-19/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/ReCOVER-COVID-19/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/ReCOVER-COVID-19/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/ReCOVER-COVID-19/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/ReCOVER-COVID-19/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/ReCOVER-COVID-19/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/ReCOVER-COVID-19/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/ReCOVER-COVID-19/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/ReCOVER-COVID-19/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/ReCOVER-COVID-19/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "0eb71c6630b49a3bf4ac65b7195931e2",
+    "url": "/ReCOVER-COVID-19/static/media/jhu_graph.0eb71c66.png"
+  },
+  {
+    "revision": "2617fd63efb85578e6e194c18ddef487",
+    "url": "/ReCOVER-COVID-19/static/media/nyt_graph.2617fd63.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/ReCOVER-COVID-19/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/ReCOVER-COVID-19/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/ReCOVER-COVID-19/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/ReCOVER-COVID-19/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/ReCOVER-COVID-19/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "037531ff16805ff830e9b325678c1d10",
+    "url": "/ReCOVER-COVID-19/static/media/usf_graph.037531ff.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReCOVER-COVID-19/precache-manifest.586cfcea6a61be86335f1e7140dbc267.js"
+  "/ReCOVER-COVID-19/precache-manifest.84ee0018b0b28f417a454c5cf340b3cb.js"
 );
 
 self.addEventListener('message', (event) => {
