e260e2885d9b8ca3e785074afeb87ef86bdb6dde
Xiaochen Yang <xiaochey@usc.edu>
2020-07-14 00:04:00 -0700

    fix layout of the pdf page

--- frontend/src/all_page_pdf/all_page_pdf.js
+++ frontend/src/all_page_pdf/all_page_pdf.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Document, Page, pdfjs} from "react-pdf";
 import "./all_page_pdf.css";
-import throttle from "lodash.throttle"
+import { SizeMe } from 'react-sizeme';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -24,16 +24,24 @@ export default function AllPages(props) {
   }
 
   const { pdf } = props;
-
   return (
-    <Document
-      file={pdf}
-      options={{ workerSrc: "pdf.worker.js" }}
-      onLoadSuccess={onDocumentLoadSuccess}
-    >
-      {Array.from(new Array(numPages), (el, index) => (
-          <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-      ))}
-    </Document>
+    <SizeMe 
+      monitorHeight
+      refreshRate={128}
+      refreshMode={"debounce"}
+      render={({ size }) =>
+      <div>
+        <Document
+          file={pdf}
+          options={{ workerSrc: "pdf.worker.js" }}
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          {Array.from(new Array(numPages), (el, index) => (
+              <Page width = {size.width} key={`page_${index + 1}`} pageNumber={index + 1} />
+          ))}
+        </Document>
+      </div>
+    }
+    />
   );
 }
