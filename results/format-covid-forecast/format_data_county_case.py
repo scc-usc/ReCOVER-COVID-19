786ee2606e6ebf3c1e9dcee3583393614940e01a
Frost Tianjian Xu <xutianjian@gmail.com>
2020-09-13 15:34:45 -0700

    Update forecast formatting scripts

--- results/format-covid-forecast/format_data_county_case.py
+++ results/format-covid-forecast/format_data_county_case.py
@@ -4,8 +4,8 @@ import csv
 import urllib.request
 import io
 
-FORECAST_DATE = datetime.date.today()
-FIRST_WEEK = datetime.date.today() + datetime.timedelta(6)
+FORECAST_DATE = datetime.datetime.today()
+FIRST_WEEK = datetime.datetime.today() + datetime.timedelta(5)
 INPUT_FILENAME = "county_forecasts_quarantine_20.csv"
 OUTPUT_FILENAME = FORECAST_DATE.strftime("%Y-%m-%d") + "-USC-SI_kJalpha.csv"
 COLUMNS = ["forecast_date", "target", "target_end_date", "location", "type", "quantile", "value"]
