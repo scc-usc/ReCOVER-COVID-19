7d4deec09cefd0b113dfb6e3c8e8ff6a380ac395
James Orme-Rogers <ormeroge@usc.edu>
2020-09-27 16:01:25 -0700

    Initial geobubble implementation

---
+++ frontend/src/cities.js
@@ -0,0 +1,24 @@
+let data = {
+    city: [
+        { "name": "Tokyo", "coordinates": [139.6917, 35.6895], "population": 37843000 },
+        { "name": "Jakarta", "coordinates": [106.8650, -6.1751], "population": 30539000 },
+        { "name": "Delhi", "coordinates": [77.1025, 28.7041], "population": 24998000 },
+        { "name": "Seoul", "coordinates": [126.9780, 37.5665], "population": 23480000 },
+        { "name": "Shanghai", "coordinates": [121.4737, 31.2304], "population": 23416000 },
+        { "name": "Karachi", "coordinates": [67.0099, 24.8615], "population": 22123000 },
+        { "name": "Beijing", "coordinates": [116.4074, 39.9042], "population": 21009000 },
+        { "name": "Mumbai", "coordinates": [72.8777, 19.0760], "population": 17712000 },
+        { "name": "Osaka", "coordinates": [135.5022, 34.6937], "population": 17444000 },
+        { "name": "Moscow", "coordinates": [37.6173, 55.7558], "population": 16170000 },
+        { "name": "Dhaka", "coordinates": [90.4125, 23.8103], "population": 15669000 },
+        { "name": "Bangkok", "coordinates": [100.5018, 13.7563], "population": 14998000 },
+        { "name": "Kolkata", "coordinates": [88.3639, 22.5726], "population": 14667000 },
+        { "name": "Istanbul", "coordinates": [28.9784, 41.0082], "population": 13287000 },
+    ],
+    minLat: -6.1751,
+    maxLat: 55.7558,
+    minLong: 37.6173,
+    maxLong: 139.6917
+}
+
+export default data
--- frontend/src/covid19map.js
+++ frontend/src/covid19map.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
-import { Circle, Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Circle, Map, Marker, Popup, TileLayer, CircleMarker, Tooltip } from "react-leaflet";
 import ModelAPI from "./modelapi";
 import { areaToStr, strToArea } from "./covid19util";
+import "leaflet/dist/leaflet.css";
+// import data from "./cities";
+import data from "./countries";
 
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4maps from "@amcharts/amcharts4/maps";
@@ -13,16 +16,19 @@ import am4geodata_canadaLow from "@amcharts/amcharts4-geodata/canadaLow";
 import am4geodata_australiaLow from "@amcharts/amcharts4-geodata/australiaLow";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
+
+
+
 // const HEAT_MAP_MIN_COLOR = "#fcbba0";
 // const HEAT_MAP_MAX_COLOR = "#66000d";
 const HEAT_MAP_MIN_COLOR = "#ffffff";
 const HEAT_MAP_MAX_COLOR = "#000000";
 const MAP_HOVER_COLOR = "#e43027";
 
-am4core.useTheme(am4themes_animated);
+// am4core.useTheme(am4themes_animated);
 
 class Covid19Map extends Component {
-  constructor() {
+  /*constructor() {
     super();
     this.modelAPI = new ModelAPI();
 
@@ -500,11 +506,71 @@ class Covid19Map extends Component {
     if (this.chart) {
       this.chart.dispose();
     }
+  }*/
+
+  getColor(d) {
+   	return d > 100 ? '#800026' :
+           d > 50  ? '#BD0026' :
+           d > 20  ? '#E31A1C' :
+           d > 10  ? '#FC4E2A' :
+           d > 5   ? '#FD8D3C' :
+           d > 2   ? '#FEB24C' :
+           d > 1   ? '#FED976' :
+                      '#FFEDA0';
   }
 
   render() {
     
-    return <div id="chartdiv"></div>;
+    //return <div id="chartdiv"></div>;
+
+    var centerLat = (data.minLat + data.maxLat) / 2;
+    var distanceLat = data.maxLat - data.minLat;
+    var bufferLat = distanceLat * 0.05;
+    var centerLong = (data.minLong + data.maxLong) / 2;
+    var distanceLong = data.maxLong - data.minLong;
+    var bufferLong = distanceLong * 0.05;
+    return (
+    	<div>
+    		<Map
+    			style={{ height: "800px", width: "100%" }}
+    			zoom={1}
+    			center={[centerLat, centerLong]}
+    			bounds={[
+    				[data.minLat - bufferLat, data.minLong - bufferLong],
+    				[data.maxLat + bufferLat, data.maxLong - bufferLong]
+    			]}
+    		>
+    			<TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+    			{data.country.map((country, k) => {
+    				return (
+    					<CircleMarker
+    						key={k}
+    						center={[country["coordinates"][0], country["coordinates"][1]]}
+    						radius={10 * Math.log(country["population"] / 1000000)}
+    						color={this.getColor(country["population"] / 1000000)}
+    						fillOpacity={0.5}
+    						stroke={false}
+    						onMapClick={this.handleMapClick}
+    					>
+    						<CircleMarker
+    							key={k}
+    							center={[country["coordinates"][0], country["coordinates"][1]]}
+    							radius={2 * Math.log(country["population"] / 1000000)}
+    							color="black"
+    							fillOpacity={1}
+    							stroke={false}
+    						></CircleMarker>
+    						<Tooltip direction="right" offset={[-8, -2]} opacity={1}>
+    							<span>{country["name"] + ": Population " + country["population"]}</span>
+    						</Tooltip>
+    					</CircleMarker>
+    					)
+    			})
+    			}
+    		</Map>
+    	</div>
+    );
   }
 }
 
