c6acfdae28cfca94a7b6d26146dee3cedc531c29
wolfaja <wolfeja@vmware.com>
2020-10-08 16:43:36 -0700

    navbar styling update

--- frontend/src/navbar/navbar.js
+++ frontend/src/navbar/navbar.js
@@ -5,7 +5,7 @@ import {
     Row,
     Col
 } from 'antd';
-import "./navbar.css";
+import styles from "./navbar.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 
@@ -13,20 +13,20 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 // class NavBar extends Component {
 const NavBar = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" className="navbar-header navbar-light">
           <Navbar.Brand href="#/"><img
                     className="logo"
                     src="https://identity.usc.edu/files/2011/12/combo_gold_white_cardinal.png"
                     alt="USC"
                 /></Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#/">COVID-19 Forecast</Nav.Link>
-              <Nav.Link href="#score">Reproduction Number</Nav.Link>
-              <Nav.Link href="#highlights">Highlights</Nav.Link>
-              <Nav.Link href="#leaderboard">Leaderboard</Nav.Link>
-              <Nav.Link href="#about">About Us</Nav.Link>
+              <Nav.Link className="navbar-link" href="#/">COVID-19 Forecast</Nav.Link>
+              <Nav.Link className="navbar-link" href="#score">Reproduction Number</Nav.Link>
+              <Nav.Link className="navbar-link" href="#highlights">Highlights</Nav.Link>
+              <Nav.Link className="navbar-link" href="#leaderboard">Leaderboard</Nav.Link>
+              <Nav.Link className="navbar-link" href="#about">About Us</Nav.Link>
             </Nav>
             <Nav>
             </Nav>
