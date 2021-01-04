77c7b03e5b6539ab37634b55239e90e80747110b
Ajitesh <ajiteshs@usc.edu>
2021-01-04 14:59:53 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReCOVER-COVID-19/precache-manifest.2fd87488e8950aa361a1b7bc693d94a8.js"
+  "/ReCOVER-COVID-19/precache-manifest.e4808c58e2a65d24e6a4124511962d8b.js"
 );
 
 self.addEventListener('message', (event) => {
---
+++ static/js/xlsx.d352f0fd.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[6],{1059:function(n,o){},1060:function(n,o){},529:function(n,o){}}]);
+//# sourceMappingURL=xlsx.d352f0fd.chunk.js.map
