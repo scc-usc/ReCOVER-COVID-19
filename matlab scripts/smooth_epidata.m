6b8ef411d8d8713a378da5809d9220486cbe046e
Ajitesh <ajiteshs@usc.edu>
2021-02-21 22:13:23 -0800

    Update smoothing method

--- matlab scripts/smooth_epidata.m
+++ matlab scripts/smooth_epidata.m
@@ -6,19 +6,21 @@ data_4_s = data_4;
 maxt = size(data_4, 2);
 date_map = ceil(((1:maxt-1) - mod(maxt-1, 7))/7);
 if isnumeric(smooth_factor)
-    cleandel = deldata;
+     cleandel = deldata;
     for cid = 1:size(data_4, 1)
         week_dat = diff(data_4(cid, mod(maxt-1, 7)+1:7:maxt)');
-        [clean_week, TF] = filloutliers(week_dat, 'linear', 'movmedian', 4, 'ThresholdFactor', 2);
-        tf_vals = find(TF);
+        [clean_week, TF] = filloutliers(week_dat, 'linear', 'movmean', 8, 'ThresholdFactor', 3);
+        [~, peak_idx] = findpeaks(week_dat);
+        tf_vals = intersect(find(TF), peak_idx);
         for jj=1:length(tf_vals)
             cleandel(date_map==tf_vals(jj), cid) = clean_week(tf_vals(jj))/7;
         end
     end
-    %cleandel = filloutliers(deldata, 'center', 'movmean', smooth_factor*2, 'ThresholdFactor', 2)';
+    cleandel = filloutliers(deldata, 'center', 'movmean', smooth_factor*2, 'ThresholdFactor', 2);
     data_4_s = [data_4(:, 1) cumsum(movmean(cleandel', smooth_factor, 2), 2)];
 else
-    cleandel = filloutliers(deldata, 'center', 'movmedian', 14);
+    %cleandel = filloutliers(deldata, 'center', 'movmedian', 14);
+    cleandel = deldata;
     for cid = 1:size(data_4, 1)
         t = smooth(cleandel(:, cid),0.1,'rloess')';
         data_4_s(cid, :) = [data_4(cid, 1) cumsum(t, 2)];
