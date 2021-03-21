d3d917aab4db21f78720a6b6ebc9119ea449ffaf
Ajitesh <ajiteshs@usc.edu>
2021-03-21 13:59:56 -0700

    Update smooth_epidata.m

--- matlab scripts/smooth_epidata.m
+++ matlab scripts/smooth_epidata.m
@@ -9,7 +9,7 @@ if isnumeric(smooth_factor)
      cleandel = deldata;
     for cid = 1:size(data_4, 1)
         week_dat = diff(data_4(cid, mod(maxt-1, 7)+1:7:maxt)');
-        [clean_week, TF] = filloutliers(week_dat, 'linear', 'movmedian', 20, 'ThresholdFactor', 2);
+        [clean_week, TF] = filloutliers(week_dat, 'linear', 'movmedian', 20, 'ThresholdFactor', 3);
         [~, peak_idx] = findpeaks([week_dat; 0]);
         tf_vals = intersect(find(TF), peak_idx);
         for jj=1:length(tf_vals)
@@ -17,7 +17,7 @@ if isnumeric(smooth_factor)
         end
     end
     deldata = cleandel;
-    cleandel = filloutliers(deldata, 'center', 'movmean', smooth_factor*2, 'ThresholdFactor', 2);
+    cleandel = filloutliers(deldata, 'center', 'movmean', smooth_factor*2, 'ThresholdFactor', 3);
     data_4_s = [data_4(:, 1) cumsum(movmean(cleandel', smooth_factor, 2), 2)];
 else
     %cleandel = filloutliers(deldata, 'center', 'movmedian', 14);
